612a26d06201241f442a892dae1d9363db4750ff
don bright <hmbright@fastmail.com>
2016-07-17 17:11:41 -0500

    add testing

--- tgawrite/maintest.c
+++ tgawrite/maintest.c
@@ -1,17 +1,66 @@
 // test of tgawrite.h, copyright don bright 2016
 // see license.txt for license
-// see also http://paulbourke.net/fractals/circlesquares/index.html
 
 #include "tgawrite.h"
 
+int patterns[7]={2,3,5,2*3,5*3,2*5,2*3*5};
+
+// see also http://paulbourke.net/fractals/circlesquares/index.html
+// see also https://www.youtube.com/watch?v=kRHKbJMm6xE
+void makepattern(uint32_t *pixels,uint16_t w,uint16_t h,int color) {
+	uint8_t pixel[4] = {0,0,0,0};
+	for (uint16_t i=0;i<h;i++) {
+		for (uint16_t j=0;j<w;j++) {
+			if (color%3==0) pixel[0] = i*i-j*j; // red
+			if (color%5==0) pixel[1] = 2*i*j; // green
+			if (color%2==0) pixel[2] = i*i+j*j; // blue
+			pixel[3] = 255; // alpha
+			// uncomment the following line to verify test failures
+			// if (color%5==0) pixel[1] = 2*i*j+1;
+			pixels[w*i+j] = *((uint32_t*)&pixel);
+		}
+		//printf("\n");
+	}
+}
+
+// test results(actual on this machine) against models (ideal)
+// 
+void tests()
+{
+	for (int i=0;i<7;i++) {
+		char fnamem[20];
+		sprintf(fnamem,"thismachine%02d.tga",patterns[i]);
+		FILE *fm = fopen( fnamem ,"rb");
+		char fnamer[20];
+		sprintf(fnamer,"testmodel%02d.tga",patterns[i]);
+		FILE *fr = fopen( fnamer ,"rb");
+		int test,mc,rc;
+		test=mc=rc=1;
+		while ( (mc!=EOF) && (rc!=EOF) ) {
+			mc = fgetc(fm);
+			rc = fgetc(fr);
+			test &= (mc==rc);
+			//printf("%i %i %i %i %i\n",i,mc==EOF,rc==EOF,mc!=EOF,rc!=EOF);
+		}
+		printf("test %s v %s: %s\n",fnamem,fnamer,test==1?"ok":"fail");
+		fclose(fm);
+		fclose(fr);
+	}
+}
+
 int main() {
-	uint16_t w = 256;
+	uint16_t w = 512;
 	uint16_t h = 256;
 	uint32_t pixels[w*h];
-	for (int i=0;i<w;i++) {
-		for (int j=0;j<h;j++) {
-			pixels[w*j+i] = i*i+j*j;
-		}
+
+	for (int i=0;i<7;i++) {
+		char fname[20];
+		sprintf(fname,"thismachine%02d.tga",patterns[i]);
+		FILE *f = fopen( fname ,"wb");
+		makepattern(pixels,w,h,patterns[i]);
+		int32_t result = tgawrite(pixels,w,h,f);
+		printf("%i bytes written to %s\n",result,fname);
+		fclose(f);
 	}
-	tgawrite("test.tga",pixels,w,h);
+	tests();
 }
--- tgawrite/tgawrite.h
+++ tgawrite/tgawrite.h
@@ -1,3 +1,5 @@
+#ifndef __tgawrite_h__
+#define __tgawrite_h__
 /*
 Copyright (c) 2016 Don Bright
 All rights reserved.
@@ -26,32 +28,38 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 /*
- write RGB pixels to tga image file
+ write RGBA pixels to tga image file BGRA.
  see also http://paulbourke.net/dataformats/tga/ , http://www.cplusplus.com
  http://stackoverflow.com/questions/16538945/writing-uncompressed-tga-to-file-in-c
  this is designed to be small, simple, easy to understand, and easy to modify.
- splitting colors using the union allows modification for various pixel formats.
- just switch the order in the union and you can use a different rgba order.
+ splitting colors using the union allows modification for various pixel formats
+ and endianness. just switch the order in the union and you can use a different
+ rgba order. FILE *F must be open in binary mode ('wb').
+ return is -1 on failure, # of pixel bytes+18 on success
 */
 
 #include <stdio.h>
 #include <stdint.h>
 
-uint32_t tgawrite( const char *fname, uint32_t *pixels, uint16_t w, uint16_t h )
+int32_t tgawrite( uint32_t *pixels, uint16_t w, uint16_t h, FILE *f )
 {
-	union pixl8 {
-		uint8_t blue,green,red,alpha;
-		uint32_t data;
+	struct data8_t {
+		uint8_t red; uint8_t green; uint8_t blue; uint8_t alpha;
+	};
+	union pixel_t {
+		struct data8_t data8;
+		uint32_t data32;
 	} pixel;
-	FILE *f = fopen( fname, "wb" );
 	uint8_t tgaheader[18] = {0,0,2,0,0,0,0,0,0,0,0,0,w,w>>8,h,h>>8,32,0};
-	fwrite( tgaheader, 1, 18, f );
-	for ( uint32_t i=0; i<w*h; pixel.data = pixels[i], i++ ) {
-			putc( pixel.blue, f );
-			putc( pixel.green, f );
-			putc( pixel.red, f );
-			putc( pixel.alpha, f );
+	if ( fwrite( tgaheader, 1, 18, f ) != 18 ) return -1;
+	for ( uint32_t i=0; i<w*h; pixel.data32 = pixels[i], i++ ) {
+			// printf("%u %u %hhu %hhu %hhu %hhu\n",i,pixel.data32,pixel.data8.blue,pixel.data8.green,pixel.data8.red,pixel.data8.alpha);
+			if (putc( pixel.data8.blue, f )==EOF) return -1;
+			if (putc( pixel.data8.green, f )==EOF) return -1;
+			if (putc( pixel.data8.red, f )==EOF) return -1;
+			if (putc( pixel.data8.alpha, f )==EOF) return -1;
 	}
-	fclose( f );
+	return 4*w*h+18;
 }
 
+#endif // __tgawrite_h__
